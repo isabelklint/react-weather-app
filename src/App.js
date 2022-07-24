960cb273a643752de149af17c2e72a12005cca2a
Isabel Klint <54295403+isabelklint@users.noreply.github.com>
2022-07-24 00:38:19 -0500

    Fixed some colors

--- src/App.js
+++ src/App.js
@@ -9,17 +9,18 @@ export default function App() {
   return (
     <div className="App">
       <div className="content-container">
-      <Top />
-      <div className="d-flex content-container justify-content-center align-items-baseline weather">
-        <div className="row d-flex">
-          <Antipodal />
-          <div className="col-12 col-md-4">
-            <img className="centerImage rotateimg90" src={require('./circlearrows.svg').default} alt='circlearrows image' />
+        <Top />
+        <div className="d-flex content-container justify-content-center align-items-baseline weather">
+          <div className="row d-flex">
+            <Antipodal />
+            <div className="col-12 col-md-4">
+              <img className="centerImage rotateimg90" src={require('./circlearrows.svg').default} alt='circlearrows image' />
+            </div>
+            <Chosen />
           </div>
-          <Chosen />
         </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   )
 }
